1f80e31683b8840f61467d01ba5700d739201627
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2020-08-24 06:57:10 -0400

    removed dead code

--- include/flex_sync/approximate_sync.h
+++ include/flex_sync/approximate_sync.h
@@ -21,19 +21,6 @@
 
 namespace flex_sync {
 
-  template <class T, class Tuple>
-  struct Index;
-
-  template <class T, class... Types>
-  struct Index<T, std::tuple<T, Types...>> {
-    static const std::size_t value = 0;
-  };
-
-  template <class T, class U, class... Types>
-  struct Index<T, std::tuple<U, Types...>> {
-    static const std::size_t value = 1 + Index<T, std::tuple<Types...>>::value;
-  };
-
   // FullIndex has two components: which type, and which topic within
   // that type
   struct FullIndex {
